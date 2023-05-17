5474197c93eb6f29706ef9fca762c6bcd859fcfa
Krystian <texen24@gmail.com>
2023-05-17 21:29:40 +0200

    created in js 5 functions and 2 listeners

---
+++ main.js
@@ -0,0 +1,60 @@
+const username = document.querySelector('#username')
+const pass = document.querySelector('#password')
+const pass2 = document.querySelector('#password2')
+const email = document.querySelector('#email')
+const sendBtn = document.querySelector('.send')
+const clearBtn = document.querySelector('.clear')
+const popup = document.querySelector('.popup')
+// const closeBtn = document.querySelector('.close')
+
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error-text')
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
+}
+
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} składać się ma z min. ${min} znaków.`)
+	}
+}
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'Hasła do siebie nie pasują.')
+	}
+}
+
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault()
+
+	checkForm([username, pass, pass2, email])
+	checkLength(username, 3)
+	checkLength(pass, 8)
+	checkLength(pass, 8)
+	checkPassword(pass, pass2)
+})
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault()
+	;[username, pass, pass2, email].forEach(el => {
+		el.value = ''
+	})
+})
