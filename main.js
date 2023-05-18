d1e18e41ce5aeb0e812d05bac7f61a2917734ec7
Krystian <texen24@gmail.com>
2023-05-18 20:23:17 +0200

    ended project

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const email = document.querySelector('#email')
 const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
-// const closeBtn = document.querySelector('.close')
+
 
 const showError = (input, msg) => {
 	const formBox = input.parentElement
@@ -41,6 +41,30 @@ const checkPassword = (pass1, pass2) => {
 	}
 }
 
+const checkMail = email => {
+	const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[a-zA-Z]{2,3})$/i
+
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'E-mail jest niepoprawny')
+	}
+}
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0;
+
+	allInputs.forEach(el => {
+		if(el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
+}
 
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
@@ -50,11 +74,14 @@ sendBtn.addEventListener('click', e => {
 	checkLength(pass, 8)
 	checkLength(pass, 8)
 	checkPassword(pass, pass2)
+	checkMail(email)
+	checkErrors()
 })
 
 clearBtn.addEventListener('click', e => {
 	e.preventDefault()
 	;[username, pass, pass2, email].forEach(el => {
 		el.value = ''
+		clearError(el)
 	})
 })
